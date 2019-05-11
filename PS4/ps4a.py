b4b00074fb87e857185c5c957eca488dd4381834
zhijuyongfeng <2604385867@qq.com>
2019-05-11 19:21:25 +0800

    almost finished

---
+++ PS4/ps4a.py
@@ -0,0 +1,69 @@
+# Problem Set 4A
+# Name: <your name here>
+# Collaborators:
+# Time Spent: x:xx
+
+def get_permutations(sequence):
+    '''
+    Enumerate all permutations of a given string
+
+    sequence (string): an arbitrary string to permute. Assume that it is a
+    non-empty string.  
+
+    You MUST use recursion for this part. Non-recursive solutions will not be
+    accepted.
+
+    Returns: a list of all permutations of sequence
+
+    Example:
+    >>> get_permutations('abc')
+    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+    Note: depending on your implementation, you may return the permutations in
+    a different order than what is listed here.
+    '''
+
+    length=len(sequence)
+    if 1==length:
+        return [sequence]
+    ans=[]
+    seq=''
+    for j in range(length):
+        if j!=0:
+            seq+=sequence[j]
+    temp=get_permutations(seq)
+    for seq in temp:
+        for j in range(length):
+            ans.append(seq[0:j]+sequence[0]+seq[j:])
+    return ans
+
+if __name__ == '__main__':
+#    #EXAMPLE
+#    example_input = 'abc'
+#    print('Input:', example_input)
+#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+#    print('Actual Output:', get_permutations(example_input))
+    
+#    # Put three example test cases here (for your sanity, limit your inputs
+#    to be three characters or fewer as you will have n! permutations for a 
+#    sequence of length n)
+
+    example_input = 'ab'
+    print('Input:', example_input)
+    print('Expected Output:', ['ab', 'ba'])
+    print('Actual Output:', get_permutations(example_input))
+
+    print('\n-------------\n')
+
+    example_input = 'abc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
+
+    print('\n-------------\n')
+
+    example_input = 'abcd'
+    ans3=['abcd', 'abdc', 'acbd', 'acdb', 'adbc', 'adcb', 'bacd', 'badc', 'bcad', 'bcda', 'bdac', 'bdca', 'cabd', 'cadb', 'cbad', 'cbda', 'cdab', 'cdba', 'dabc', 'dacb', 'dbac', 'dbca', 'dcab', 'dcba']
+    print('Input:', example_input)
+    print('Expected Output:', ans3)
+    print('Actual Output:', get_permutations(example_input))
---
+++ PS4/ps4b.py
@@ -0,0 +1,278 @@
+# Problem Set 4B
+# Name: <your name here>
+# Collaborators:
+# Time Spent: x:xx
+
+import string
+
+### HELPER CODE ###
+def load_words(file_name):
+    '''
+    file_name (string): the name of the file containing 
+    the list of words to load    
+    
+    Returns: a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    '''
+    print("Loading word list from file...")
+    # inFile: file
+    inFile = open(file_name, 'r')
+    # wordlist: list of strings
+    wordlist = []
+    for line in inFile:
+        wordlist.extend([word.lower() for word in line.split(' ')])
+    print("  ", len(wordlist), "words loaded.")
+    return wordlist
+
+def is_word(word_list, word):
+    '''
+    Determines if word is a valid word, ignoring
+    capitalization and punctuation
+
+    word_list (list): list of words in the dictionary.
+    word (string): a possible word.
+    
+    Returns: True if word is in word_list, False otherwise
+
+    Example:
+    >>> is_word(word_list, 'bat') returns
+    True
+    >>> is_word(word_list, 'asdf') returns
+    False
+    '''
+    word = word.lower()
+    word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
+    return word in word_list
+
+def get_story_string():
+    """
+    Returns: a story in encrypted text.
+    """
+    f = open("story.txt", "r")
+    story = str(f.read())
+    f.close()
+    return story
+
+### END HELPER CODE ###
+
+WORDLIST_FILENAME = 'words.txt'
+
+class Message(object):
+    def __init__(self, text):
+        '''
+        Initializes a Message object
+                
+        text (string): the message's text
+
+        a Message object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+        '''
+        object.__init__(self)
+        self.message_text=text
+        self.vaild_words=load_words(WORDLIST_FILENAME)
+
+    def get_message_text(self):
+        '''
+        Used to safely access self.message_text outside of the class
+        
+        Returns: self.message_text
+        '''
+        return self.message_text
+
+    def get_valid_words(self):
+        '''
+        Used to safely access a copy of self.valid_words outside of the class.
+        This helps you avoid accidentally mutating class attributes.
+        
+        Returns: a COPY of self.valid_words
+        '''
+        return self.vaild_words[:]
+
+    def build_shift_dict(self, shift):
+        '''
+        Creates a dictionary that can be used to apply a cipher to a letter.
+        The dictionary maps every uppercase and lowercase letter to a
+        character shifted down the alphabet by the input shift. The dictionary
+        should have 52 keys of all the uppercase letters and all the lowercase
+        letters only.        
+        
+        shift (integer): the amount by which to shift every letter of the 
+        alphabet. 0 <= shift < 26
+
+        Returns: a dictionary mapping a letter (string) to 
+                 another letter (string). 
+        '''
+        ans={}
+        upper=string.ascii_uppercase
+        length=len(upper)
+        for i in range(length):
+            ans[upper[i]]=upper[(i+shift)%length]
+        lower=string.ascii_lowercase
+        for i in range(length):
+            ans[lower[i]]=lower[(i+shift)%length]
+        return ans
+
+    def apply_shift(self, shift):
+        '''
+        Applies the Caesar Cipher to self.message_text with the input shift.
+        Creates a new string that is self.message_text shifted down the
+        alphabet by some number of characters determined by the input shift        
+        
+        shift (integer): the shift with which to encrypt the message.
+        0 <= shift < 26
+
+        Returns: the message text (string) in which every character is shifted
+             down the alphabet by the input shift
+        '''
+        d=self.build_shift_dict(shift)
+        length=len(self.message_text)
+        ans=''
+        for i in range(length):
+            ch=self.message_text[i]
+            ans+=d.get(ch,ch)
+        return ans
+
+class PlaintextMessage(Message):
+    def __init__(self, text, shift):
+        '''
+        Initializes a PlaintextMessage object        
+        
+        text (string): the message's text
+        shift (integer): the shift associated with this message
+
+        A PlaintextMessage object inherits from Message and has five attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+            self.shift (integer, determined by input shift)
+            self.encryption_dict (dictionary, built using shift)
+            self.message_text_encrypted (string, created using shift)
+
+        '''
+        Message.__init__(self,text)
+        self.shift=shift
+        self.encryption_dict=self.build_shift_dict(shift)
+        self.message_text_encrypted=self.apply_shift(shift)
+
+    def get_shift(self):
+        '''
+        Used to safely access self.shift outside of the class
+        
+        Returns: self.shift
+        '''
+        return self.shift
+
+    def get_encryption_dict(self):
+        '''
+        Used to safely access a copy self.encryption_dict outside of the class
+        
+        Returns: a COPY of self.encryption_dict
+        '''
+        return self.encryption_dict.copy()
+
+    def get_message_text_encrypted(self):
+        '''
+        Used to safely access self.message_text_encrypted outside of the class
+        
+        Returns: self.message_text_encrypted
+        '''
+        return self.message_text_encrypted
+
+    def change_shift(self, shift):
+        '''
+        Changes self.shift of the PlaintextMessage and updates other 
+        attributes determined by shift.        
+        
+        shift (integer): the new shift that should be associated with this message.
+        0 <= shift < 26
+
+        Returns: nothing
+        '''
+        self.shift=shift
+        self.encryption_dict=self.build_shift_dict(shift)
+        self.message_text_encrypted=self.apply_shift(shift)
+
+class CiphertextMessage(Message):
+    def __init__(self, text):
+        '''
+        Initializes a CiphertextMessage object
+                
+        text (string): the message's text
+
+        a CiphertextMessage object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+        '''
+        Message.__init__(self,text)
+
+    def decrypt_message(self):
+        '''
+        Decrypt self.message_text by trying every possible shift value
+        and find the "best" one. We will define "best" as the shift that
+        creates the maximum number of real words when we use apply_shift(shift)
+        on the message text. If s is the original shift value used to encrypt
+        the message, then we would expect 26 - s to be the best shift value 
+        for decrypting it.
+
+        Note: if multiple shifts are equally good such that they all create 
+        the maximum number of valid words, you may choose any of those shifts 
+        (and their corresponding decrypted messages) to return
+
+        Returns: a tuple of the best shift value used to decrypt the message
+        and the decrypted message text using that shift value
+        '''
+        chars=string.ascii_letters
+        max_real_word=0
+        best_shift=0
+        text=''
+        for i in range(26):
+            s=self.apply_shift(i)
+            
+            temp_real_word=0
+            word=''
+            for j in s:
+                if j in chars:
+                    word+=j
+                else:
+                    if is_word(self.vaild_words,word):
+                        temp_real_word+=1
+                    word=''
+            if len(word)>0:
+                if is_word(self.vaild_words,word):
+                        temp_real_word+=1
+            if temp_real_word>max_real_word:
+                max_real_word=temp_real_word
+                best_shift=i
+                text=s
+        return (best_shift,text)
+
+
+if __name__ == '__main__':
+
+#    #Example test case (PlaintextMessage)
+    plaintext = PlaintextMessage('hello', 2)
+    print('Input: hello,2')
+    print('Expected Output: jgnnq')
+    print('Actual Output:', plaintext.get_message_text_encrypted())
+    print('---------------------')
+#    #Example test case (CiphertextMessage)
+    ciphertext = CiphertextMessage('jgnnq')
+    print('Input: jgnnq')
+    print('Expected Output:', (24, 'hello'))
+    print('Actual Output:', ciphertext.decrypt_message())
+    print('---------------------')
+    #TODO: WRITE YOUR TEST CASES HERE
+
+    #TODO: best shift value and unencrypted story 
+    plaintext=PlaintextMessage('Hello,World!',4)
+    print('Input: Hello,World!,4')
+    print('Expected Output: Lipps,Asvph!')
+    print('Actual Output:', plaintext.get_message_text_encrypted())
+    print('---------------------')
+
+    ciphertext=CiphertextMessage(get_story_string())
+    print('Input: ',ciphertext.get_message_text())
+    print('Expected Output: ',(12,'Jack Florey is a mythical character created on the spur of a moment to help cover an insufficiently planned hack. He has been registered for classes at MIT twice before, but has reportedly never passed aclass. It has been the tradition of the residents of East Campus to become Jack Florey for a few nights each year to educate incoming students in the ways, means, and ethics of hacking.'))
+    print('Actual Output: {}'.format(ciphertext.decrypt_message()))
